57070de1f51d4c6a9069fed573ec5cbfeb9da4c4
alex <desorbaix@free.fr>
2020-10-14 14:41:54 +0200

    creation du composant d'edition

---
+++ src/App/components/MemeEditor/MemeEditor.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import styles from './MemeEditor.module.scss';
+
+const MemeEditor = () => (
+  <div className={styles.MemeEditor}>
+    <div className={styles.titre}>
+      MemeEditor Component
+  </div>
+  </div>
+);
+
+// MemeEditor.propTypes = {};
+
+// MemeEditor.defaultProps = {};
+
+export default MemeEditor;
--- src/App/components/MemeViewer/MemeViewer.js
+++ src/App/components/MemeViewer/MemeViewer.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-
+import '../../../../node_modules/bootstrap/dist/css/bootstrap.css';
 function MemeViever(props) {
-    const [state, setstate] = useState({ maValue: props.value });
+    const [state, setstate] = useState({valeur2:"zekjdshcs", maValue: props.value });
     console.log(props, state);
     return (
         <div className={"hgefid" + props.value}>
             Mon Viewer :: props : {props.value}:: state:{JSON.stringify(state)}
+            <br/>
+            <button className="btn btn-primary" 
+                    onClick={function(){setstate({...state,maValue:"nouvelle valeur"})}}>edit state</button>
             <hr/>
         </div>);
 }
